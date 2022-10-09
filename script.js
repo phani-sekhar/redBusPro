47a0fb5f4a77b583e8f3311d5d9e21635ed43365
phani-sekhar <vphanisekhar@gmail.com>
2022-10-09 10:42:03 +0530

    added coupon

---
+++ script.js
@@ -0,0 +1,8 @@
+function loadCoupon() {
+  document.getElementById("coupon").style.visibility = "visible";
+  document.getElementById("total").style.opacity = 0.6;
+}
+function closeCoupon() {
+  document.getElementById("coupon").style.visibility = "hidden";
+  document.getElementById("total").style.opacity = 1;
+}
