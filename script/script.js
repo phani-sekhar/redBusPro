cc01c586c8028d07be9841a414e977ab9e63eb17
phani-sekhar <vphanisekhar@gmail.com>
2022-10-14 15:33:55 +0530

    added dark and light theme

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// Coupon Popup
+
 function loadCoupon() {
   if (window.matchMedia("(max-width: 896px)").matches) {
     document.getElementById("coupon-container").style.visibility = "hidden";
@@ -10,4 +12,28 @@ function loadCoupon() {
 function closeCoupon() {
   document.getElementById("coupon-container").style.visibility = "hidden";
   document.body.style.overflow = "scroll";
+}
+
+// Darkmode
+function changeMode() {
+ 
+  let darkBody = document.body;
+  darkBody.classList.toggle("dark-body");
+
+  let darkAD = document.getElementById("appAd");
+  darkAD.classList.toggle("dark-body");
+  let darkPromise = document.getElementById("promise");
+  darkPromise.classList.toggle("dark-body");
+
+  let darkAwards = document.getElementById("awards");
+  darkAwards.classList.toggle("dark-body");
+
+  let darkFlags = document.getElementById("flags");
+  darkFlags.classList.toggle("dark-body");
+
+  let darkNumbers = document.getElementById("number");
+  darkNumbers.classList.toggle("dark-body");
+
+  let darkBus = document.getElementById("buses");
+  darkBus.classList.toggle("dark-body");
 }
